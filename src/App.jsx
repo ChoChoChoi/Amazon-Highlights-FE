4c938ff3a41a70246f20bb05de17d36d048919bc
seungwon2 <dandelionsw2@ewhain.net>
2023-01-13 21:05:55 +0900

    :sparkles: Feat : finish UI

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,29 @@ import { API, graphqlOperation } from "aws-amplify";
 import { listUsers } from "./graphql/queries";
 
 import { styled } from "@mui/material/styles";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Stack from "@mui/material/Stack";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
-import StepContent from "@mui/material/StepContent";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import Slide from "@mui/material/Slide";
 import { createUser } from "./graphql/mutations";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Collapse from "@mui/material/Collapse";
+import { TransitionGroup } from "react-transition-group";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Backdrop from "@mui/material/Backdrop";
 
 const theme = createTheme({
   palette: {
@@ -61,28 +61,43 @@ const Item = styled(Paper)(({ theme }) => ({
   boxShadow: "none",
 }));
 
-const steps = [
-  "Select master blaster campaign settings",
-  "Create an ad group",
-  "Create an ad",
+const steps = ["Name", "Email", "Topic"];
+
+const tags = [
+  "Compute",
+  "Database",
+  "Analytics",
+  "Machine Learning",
+  "Internet of Things",
+  "Storage",
+  "Front-end Web & Mobile",
+  "Networking",
 ];
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function App() {
-  const [user, setUsers] = useState([]);
+  const [user, setUsers] = useState({ email: "", name: "" });
   const [step, setStep] = useState(1);
+  const [tagsInBasket, setTagsInBasket] = useState(tags);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
-    fetchUsers();
+    // fetchUsers();
   }, []);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      name: data.get("name"),
-      email: data.get("email"),
-    });
-  };
 
   const ColorButton = styled(Button)(({ theme }) => ({
     color: theme.palette.getContrastText(theme.palette.secondary.main),
@@ -95,33 +110,110 @@ export default function App() {
     },
   }));
 
-  async function fetchUsers() {
-    try {
-      const postData = await API.graphql({ query: listUsers });
-      setUsers(postData.data.listUsers.items);
-    } catch (err) {
-      console.log({ err });
-    }
-  }
-
-  async function registerUser() {
+  async function createUserFunction() {
     // https://aws.amazon.com/blogs/mobile/build-real-time-multi-user-experiences-using-graphql-on-aws-amplify/
+    //https://github.com/SamWSoftware/amplify-react-tutorial-project/blob/amplify-upload/src/App.jsx
     try {
-      const registerUserdata = await API.graphql(
-        graphqlOperation(createUser, { input: { id: user.email , name: user.name , tag: user.tag. } })
-      );
+      const userdata = {
+        // id: unstable_useId(),
+        name: user.name,
+        email: user.email,
+        tag: tagsInBasket,
+      };
+      console.log(userdata);
+      await API.graphql(graphqlOperation(createUser, { input: userdata }));
     } catch (e) {
       console.log({ e });
     }
   }
 
+  const handleonClick = () => {
+    if (step === 1 || step === 2) setStep(step + 1);
+    if (step === 3) {
+      setOpen(true);
+      console.log(open);
+      createUserFunction();
+      setStep(1);
+    }
+  };
+
+  function renderItem({ item, handleRemoveTag }) {
+    return (
+      <ListItem
+        secondaryAction={
+          <IconButton
+            edge="end"
+            aria-label="delete"
+            title="Delete"
+            onClick={() => handleRemoveTag(item)}
+          >
+            <DeleteIcon />
+          </IconButton>
+        }
+      >
+        <ListItemText primary={item} />
+      </ListItem>
+    );
+  }
+  const handleAddTag = () => {
+    const nextHiddenItem = tags.find((i) => !tagsInBasket.includes(i));
+    if (nextHiddenItem) {
+      setTagsInBasket((prev) => [nextHiddenItem, ...prev]);
+    }
+  };
+
+  const handleRemoveTag = (item) => {
+    setTagsInBasket((prev) => [...prev.filter((i) => i !== item)]);
+  };
+
+  const addTagButton = (
+    <Button
+      variant="contained"
+      disabled={tagsInBasket.length >= tags.length}
+      onClick={handleAddTag}
+    >
+      Undo
+    </Button>
+  );
+
+  function handlePrev() {
+    setStep(step - 1);
+  }
+  const handleFormChange = (e) => {
+    setUsers({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+    // console.log(user);
+  };
+  const text = [
+    { text: "What should we call you?", var: "name" },
+    { text: "What's your email address?", var: "email" },
+    { text: "What are your interests?", var: "tag" },
+  ];
+
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ flexGrow: 1, minHeight: "100vh" }} height="100vh">
-        <Grid container sx={{ height: "100vh" }}>
-          <Grid item xs={12} sm={8} alignItems="center" justifyContent="center">
+        <Grid container sx={{ minheight: "100vh" }}>
+          <Grid
+            item
+            xs={12}
+            sm={8}
+            alignItems="center"
+            justifyContent="center"
+            minHeight="100vh"
+          >
             <Box
-              bgcolor={theme.palette.primary.main}
+              // bgcolor={theme.palette.primary.main}
+              sx={{
+                // background: "conic-gradient(from 295deg, #FFAD0D, #010101)",
+                /* Created with https://www.css-gradient.com */
+                background:
+                  "radial-gradient(ellipse at center, #B08030, #030425)",
+                /* Created with https://www.css-gradient.com */
+              }}
+              // fec934
               component={Grid}
               xs={12}
               display={{ xs: "none", lg: "flex" }}
@@ -129,10 +221,49 @@ export default function App() {
               justifyContent="center"
               alignItems="center"
               minHeight="100vh"
+              // height="100vh"
+              height="100%"
+              columnSpacing={12}
             >
-              <Typography variant="h4" gutterBottom>
-                Amazon Highlights
-              </Typography>
+              <Card
+                sx={{
+                  minHeight: "280px",
+                  width: "100%",
+                  height: "auto",
+                  maxWidth: "300px",
+                  marginRight: "10%",
+                  background: "transparent",
+                  boxShadow: "none",
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  minHeight="280px"
+                  image="/mockup.png"
+                />
+              </Card>
+              <Box>
+                <Typography variant="h3" gutterBottom fontWeight={700}>
+                  Amazon Highlights
+                </Typography>
+                <Typography sx={{ width: "50vh" }} gutterTop fontWeight={400}>
+                  <br />
+                  Amazon Highlights aims to assist Amazon employees to better
+                  self-serve and provide a personalized, first glance to newly
+                  released information across multiple channels. By connecting
+                  to available information distribution channels (ex. youtube),
+                  <br /> <br />
+                  Amazon Highlights would first scan newly uploaded content on a
+                  weekly basis, summarize the information and provide it to the
+                  user as a newsletter format if it matches the user defined
+                  keyword. <br /> <br />
+                  With Amazon Highlight, users will not have to manually search
+                  and review information spread across multiple channels. On top
+                  of being able to summarize text, Amazon Highlights also
+                  provides a unique feature that also summarizes video content
+                  such as broadcast or meeting recordings.
+                </Typography>
+              </Box>
             </Box>
           </Grid>
           <Grid
@@ -180,22 +311,118 @@ export default function App() {
               >
                 <Grid item>
                   <Typography gutterBottom fontWeight={700}>
-                    Subscribe Amazon Highlights
+                    {text[step - 1].text}
                   </Typography>
                 </Grid>
-                <Grid item>
-                  <TextField
-                    id="outlined-basic"
-                    label="Name"
-                    variant="outlined"
-                    sx={{
-                      width: { sm: 200, md: 400 },
-                    }}
-                  />
-                </Grid>
+                {step === 1 && (
+                  <>
+                    <Grid item paddingTop={2}>
+                      <TextField
+                        id="outlined-basic"
+                        label={text[step - 1].var}
+                        variant="outlined"
+                        sx={{
+                          width: { xs: 300, md: 400 },
+                        }}
+                        name="name"
+                        onChange={handleFormChange}
+                      />
+                    </Grid>
+                  </>
+                )}
+                {step === 2 && (
+                  <>
+                    <Grid item paddingTop={2}>
+                      <TextField
+                        id="outlined-basic"
+                        label={text[step - 1].var}
+                        variant="outlined"
+                        sx={{
+                          width: { xs: 300, md: 400 },
+                        }}
+                        name="email"
+                        onChange={handleFormChange}
+                      />
+                    </Grid>
+                  </>
+                )}
+                {step === 3 && (
+                  <>
+                    {addTagButton}
+                    <Box sx={{ mt: 1 }}>
+                      <List>
+                        <TransitionGroup>
+                          {tagsInBasket.map((item) => (
+                            <Collapse key={item}>
+                              {renderItem({ item, handleRemoveTag })}
+                            </Collapse>
+                          ))}
+                        </TransitionGroup>
+                      </List>
+                    </Box>
+                    <Modal
+                      aria-labelledby="transition-modal-title"
+                      aria-describedby="transition-modal-description"
+                      open={open}
+                      onClose={handleClose}
+                      closeAfterTransition
+                      BackdropComponent={Backdrop}
+                      BackdropProps={{
+                        timeout: 500,
+                      }}
+                    >
+                      <Fade in={open}>
+                        <Box sx={style}>
+                          <Typography
+                            id="transition-modal-title"
+                            variant="h6"
+                            component="h2"
+                          >
+                            Text in a modal
+                          </Typography>
+                          <Typography
+                            id="transition-modal-description"
+                            sx={{ mt: 2 }}
+                          >
+                            Duis mollis, est non commodo luctus, nisi erat
+                            porttitor ligula.
+                          </Typography>
+                        </Box>
+                      </Fade>
+                    </Modal>
+                  </>
+                )}
               </Grid>
-              <Grid item paddingBottom={8}>
-                <ColorButton sx={{ width: 200, padding: 1 }}>Next</ColorButton>
+              <Grid
+                item
+                paddingBottom={8}
+                container
+                direction="column"
+                alignItems="flex-start"
+              >
+                {step !== 3 && (
+                  <ColorButton
+                    sx={{ width: 200, padding: 1 }}
+                    onClick={handleonClick}
+                  >
+                    Next
+                  </ColorButton>
+                )}
+                {step === 3 && (
+                  <ColorButton
+                    sx={{ width: 200, padding: 1 }}
+                    onClickCapture={handleonClick}
+                    onClick={handleonClick}
+                  >
+                    Submit
+                  </ColorButton>
+                )}
+
+                {step !== 1 && (
+                  <>
+                    <Button onClick={handlePrev}>previous</Button>
+                  </>
+                )}
               </Grid>
             </Grid>
             <Grid item>
